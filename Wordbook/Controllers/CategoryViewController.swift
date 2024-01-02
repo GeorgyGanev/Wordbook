144f1d943c1a2b47b4b0959089087bdb99a5f681
Georgy Ganev <106162345+GeorgyGanev@users.noreply.github.com>
2024-01-02 18:37:50 +0200

    Add option to cancel alert for adding new language

--- Wordbook/Controllers/CategoryViewController.swift
+++ Wordbook/Controllers/CategoryViewController.swift
@@ -6,40 +6,85 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var wordbookArray = [Wordbook]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadWordbooks()
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+    // MARK: - TableView DataSource Methods
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return wordbookArray.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordbookCell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = wordbookArray[indexPath.row].name
 
         return cell
     }
-    */
+ 
+    
+    //MARK: - TableView Data Manipulation Methods
+    
+    func saveWordbook() {
+        do {
+            try context.save()
+        } catch {
+            print("error saving new wordbook: \(error.localizedDescription)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadWordbooks () {
+        let request: NSFetchRequest<Wordbook> = Wordbook.fetchRequest()
+        do {
+            try wordbookArray = context.fetch(request)
+        } catch {
+            print("error loading wordbooks: \(error.localizedDescription)")
+        }
+        tableView.reloadData()
+    }
+    
+    //MARK: - TableVIew Delegate Methods
+    
+    //MARK: - Add New Wordbook Category
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Language", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newWordbook = Wordbook(context: self.context)
+            newWordbook.name = textField.text
+            self.wordbookArray.append(newWordbook)
+            self.saveWordbook()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "New Language"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+  
 
     /*
     // Override to support conditional editing of the table view.
@@ -61,20 +106,6 @@ class CategoryViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
